0a6090e8a12cbc2b455571f2d0be8c6fad963da4
Daniel Balloch <43333774+danielpbdesign@users.noreply.github.com>
2022-12-28 16:43:40 +1300

    add video

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,13 +10,14 @@ max-width: 100vw;
 `
 
 export default function Index(){
-    // let VideoURL = "video/2023_Glacier_Hybrid_Tundra_1.mp4"
+    let VideoURL = "video/2023_Glacier_Sequoia.mp4"
     return (
         <Wrapper>
-            {/* <video
+            <video
+                controls
                 disablePictureInPicture
                 id="BgVideo"
-                title="Tundra Reel"
+                title="Sequoia Reel"
                 height="100%"
                 width="100%"
                 loop
@@ -25,11 +26,11 @@ export default function Index(){
                 playsInline 
                 preload="auto"
                 type="video/mp4"
-                placeholder="../content/Tundra.jpg"
+                placeholder="../content/sequoia.jpg"
             >
                 <source src={VideoURL} type="video/mp4"/>
-            </video> */}
-            <StaticImage src="../content/Tundra.jpg"/>
+            </video>
+            <StaticImage src="../content/sequoia.jpg"/>
         </Wrapper>
     )
 }
